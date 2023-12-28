c677e9ff85fb8111c4b45aa47fd23e0382fa014e
itsmagical <itsmagical@163.com>
2023-12-28 14:36:27 +0800

    Update util.dart
    Util增加空判断

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -10,6 +10,23 @@ class Util {
     return null != value;
   }
 
+  /// @return true object is null
+  static bool isNull(Object value) {
+    return null == value;
+  }
+
+  static bool isEmpty(dynamic value) {
+    if (value == null) {
+      return true;
+    }
+    if (value is String) {
+      return value == null || value.isEmpty;
+    }
+    if (value is Iterable) {
+      return null == value || value.length == 0;
+    }
+  }
+
   /// @return true 字符串不为空
   static bool isNotEmptyText(String text) {
     return text != null && text != '';
