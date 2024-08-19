2cbc1c3ef71b6ab872c1cbd65a607ed5f3040a27
itsmagical <Itsmagical163>
2024-08-19 16:42:43 +0800

    增加获取月的天数方法

--- lib/util/date_util.dart
+++ lib/util/date_util.dart
@@ -467,6 +467,38 @@ class DateUtil {
         DateTime.fromMillisecondsSinceEpoch(millis, isUtc: isUtc));
   }
 
+  /// 月的天数
+  static int getDayOfMonth(DateTime dateTime) {
+    int month = dateTime.month;
+    switch(month) {
+      case 1:
+        return 31;
+      case 2:
+        return isLeapYearByYear(dateTime.year) ? 29 : 28;
+      case 3:
+        return 31;
+      case 4:
+        return 30;
+      case 5:
+        return 31;
+      case 6:
+        return 30;
+      case 7:
+        return 31;
+      case 8:
+        return 31;
+      case 9:
+        return 30;
+      case 10:
+        return 31;
+      case 11:
+        return 30;
+      case 12:
+        return 31;
+    }
+    return 0;
+  }
+
   /// get day of year.
   /// 在今年的第几天.
   static int getDayOfYear(DateTime dateTime) {
