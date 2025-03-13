a2a2a4597a6cec91cefb47ff05b07548d376de82
yy1234 <270089758@qq.com>
2025-03-13 10:34:06 +0800

    添加公共线条

---
+++ lib/widget/common_widget/divider_line.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class DividerLine extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Divider(
+      height: 0.1,
+      thickness: 0.3,
+      color: const Color(0xff999999),
+    );
+  }
+}
